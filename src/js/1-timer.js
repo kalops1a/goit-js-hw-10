5a576d2099b195a8b8973fedae13a974fe196381
kalops1a <vadymduchenko@gmail.com>
2024-03-27 22:54:23 +0100

    2

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -11,3 +11,19 @@ const options = {
     console.log(selectedDates[0]);
   },
 };
+
+let userSelectedDate = null;
+
+const onCloseHandler = function (selectedDates) {
+
+    if (selectedDates.length > 0) {
+        userSelectedDate = selectedDates[0];
+        const currentDate = new Date();
+        if (userSelectedDate > currentDate) {
+            
+            document.getElementById('startButton').disabled = false;
+        } else {
+            document.getElementById(`startButton`).disabled = true;
+        window.alert("Please choose a date in the future");}
+    }
+}
